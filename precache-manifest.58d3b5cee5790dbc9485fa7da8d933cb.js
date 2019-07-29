ac512af8f7c518dca99c51308a7a045141ca8b18
Stephen Anderson <anderssj@usc.edu>
2019-07-29 16:30:41 -0700

    Updates

---
+++ precache-manifest.58d3b5cee5790dbc9485fa7da8d933cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13c5fc3d237927ddc5985a50ba382efa",
+    "url": "/simplr/index.html"
+  },
+  {
+    "revision": "67c8060b982d6922915a",
+    "url": "/simplr/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "da192ee1fd005d5882db",
+    "url": "/simplr/static/css/main.6f8ffa95.chunk.css"
+  },
+  {
+    "revision": "67c8060b982d6922915a",
+    "url": "/simplr/static/js/2.992b7481.chunk.js"
+  },
+  {
+    "revision": "da192ee1fd005d5882db",
+    "url": "/simplr/static/js/main.0179127d.chunk.js"
+  },
+  {
+    "revision": "2a0cc84a1abcaf1452ef",
+    "url": "/simplr/static/js/runtime~main.a09440d8.js"
+  }
+]);
